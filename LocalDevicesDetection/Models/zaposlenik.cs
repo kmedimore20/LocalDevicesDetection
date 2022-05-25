d27cfad3180ced079f8b8751c80107e44c0e6980
Karlo <Karlo@DESKTOP-1ECLT22>
2022-05-25 17:04:32 +0200

    Add project files.

---
+++ LocalDevicesDetection/Models/zaposlenik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDevicesDetection.Models
+{
+    public abstract class zaposlenik
+    {
+        public int ID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Uloga { get; set; }
+    }
+
+    public class voditelj : zaposlenik
+    {
+        public string objekt { get; set; }
+    }
+
+    public class zastitar : zaposlenik
+    {
+        public int broj_zatvorenika { get; set; }
+    }
+}
